b9d4dd1b46fafc81c30c38fb11f50df0bc1ca7a7
dincengincan <engincandinc@gmail.com>
2019-12-13 21:27:07 +0300

    Updates

---
+++ precache-manifest.dfb1b46c49d8b527cf0d0140972ff068.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f1e1f264aa11e0d0bfefcbb7d8290a",
+    "url": "/todo-app-react-redux/index.html"
+  },
+  {
+    "revision": "c4064e96b2b7f6b3f700",
+    "url": "/todo-app-react-redux/static/css/main.e6fe5204.chunk.css"
+  },
+  {
+    "revision": "6e6569401c4b406b8404",
+    "url": "/todo-app-react-redux/static/js/2.85ce816c.chunk.js"
+  },
+  {
+    "revision": "c4064e96b2b7f6b3f700",
+    "url": "/todo-app-react-redux/static/js/main.27e81b76.chunk.js"
+  },
+  {
+    "revision": "f61c45127f26870224ab",
+    "url": "/todo-app-react-redux/static/js/runtime-main.1f669618.js"
+  }
+]);
